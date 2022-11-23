575b12592c2acceec153cdad2b6a83c23e3e3cff
Cameron <113189878+camerongonsalves@users.noreply.github.com>
2022-11-23 10:09:17 -0500

    Add files via upload

---
+++ Q1Pythagoras.py
@@ -0,0 +1,13 @@
+a = input("Please enter the length of A!")
+a = int(a)
+print()
+b = input("Please enter the length of B!")
+b = int(b)
+a2 = a*a
+b2 = b*b
+c2 = a2+b2
+import math
+c = math.sqrt(c2)
+print()
+print ("C:")
+print(c)
